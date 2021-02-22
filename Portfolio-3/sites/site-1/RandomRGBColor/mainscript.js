ea10bc815421c11d5845bd07a44c50bf6334b349
Vladimir Saenko <vova.saenko1@gmail.com>
2021-02-22 16:48:17 +0200

    create portfolio and add first site-1
    create portfolio and add first site-"Generator-Random-RGB-Color"

---
+++ Portfolio-3/sites/site-1/RandomRGBColor/mainscript.js
@@ -0,0 +1,89 @@
+
+"use strict"
+
+let box = document.getElementById("mainBox");
+let inputRgb = document.getElementById("mainInput");
+let getRandomRGBColorBtn = document.getElementById("btnka");
+let infinityColorStreamBtn = document.getElementById("infColorStream");
+
+infinityColorStreamBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+infinityColorStreamBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+
+function getRandomRGBColor() {
+
+    box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    inputRgb.value = box.style.background + ';';
+    inputRgb.style.color = box.style.background;
+    getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    getRandomRGBColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+
+    ifEqualyColors();
+
+    if(getRandomRGBColorBtn.style.background == box.style.background ) {
+
+        box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        inputRgb.value = box.style.background + ';';
+        inputRgb.style.color = box.style.background;
+        getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    }
+
+    if(getRandomRGBColorBtn.style.color == getRandomRGBColorBtn.style.background || getRandomRGBColorBtn.style.color == inputRgb.style.color ||
+       getRandomRGBColorBtn.style.color == box.style.background) {
+
+        box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        inputRgb.value = box.style.background + ';';
+        inputRgb.style.color = box.style.background;
+        getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        getRandomRGBColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    }
+}
+
+function ifEqualyColors() {
+
+    if(getRandomRGBColorBtn.style.background == box.style.background ) {
+
+        box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        inputRgb.value = box.style.background + ';';
+        inputRgb.style.color = box.style.background;
+        getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    }
+
+    if(getRandomRGBColorBtn.style.color == getRandomRGBColorBtn.style.background || getRandomRGBColorBtn.style.color == inputRgb.style.color ||
+       getRandomRGBColorBtn.style.color == box.style.background) {
+
+        box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        inputRgb.value = box.style.background + ';';
+        inputRgb.style.color = box.style.background;
+        getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+        getRandomRGBColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    }
+}
+
+function activateInfinityStreamColor() {
+
+    setInterval(
+
+        function getRandomRGBColor2() {
+            box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+            // inputRgb.value = box.style.background + ';';
+            // inputRgb.style.color = box.style.background;
+            inputRgb.style.display = "none";
+            getRandomRGBColorBtn.style.display = "none";
+            infinityColorStreamBtn.style.display = "none";
+            let body = document.body;
+            body.style.cursor = "none";
+        }, 1000
+    );
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+getRandomRGBColor();
+ifEqualyColors();
+setInterval(function infinityStreamInColorText() {
+    infinityColorStreamBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+    infinityColorStreamBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
+}, 1000);
+
