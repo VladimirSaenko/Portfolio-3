0c09c4b5fa87ad75adc200e7020aff6d11733fd4
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-20 17:10:47 +0200

    center element if media & del old nonwork icon in site-5
    center element if media & del old nonwork icon in site-5

--- sites/site-5/Witcher3Countrys-SVG-Icon-animation-example/mainscript.js
+++ sites/site-5/Witcher3Countrys-SVG-Icon-animation-example/mainscript.js
@@ -9,6 +9,50 @@ let title1 = document.getElementById("title1");
 let title2 = document.getElementById("title2");
 let title3 = document.getElementById("title3");
 
+function otherWise() {
+    if(window.matchMedia("(max-width: 600px)")) {
+        unicorn.addEventListener('click',function() {
+            body.style.background = "yellow";
+            body.style.display = "flex";
+            body.style.justifyContent = "center";
+            body.style.alignItems = "center";
+            title1.style.visibility = "visible";
+            title1.innerHTML = "kaedwen";
+            title1.style.color = "black";
+            sun.style.display = "none";
+            eagle.style.display = "none";
+            title2.style.visibility = "hidden";
+            title3.style.visibility = "hidden";
+        })
+        eagle.addEventListener('click', function() {
+            body.style.display = "flex";
+            body.style.justifyContent = "center";
+            body.style.alignItems = "center";
+            body.style.background = "red";
+            title2.style.visibility = "visible";
+            title2.style.color = "white";
+            title2.innerHTML = "redania";
+            sun.style.display = "none";
+            unicorn.style.display = "none";
+            title1.style.visibility = "hidden";
+            title3.style.visibility = "hidden";
+        })
+        sun.addEventListener('click', function() {
+            body.style.display = "flex"; 
+            body.style.justifyContent = "center";
+            body.style.alignItems = "center";
+            body.style.background = "black"
+            title3.style.visibility = "visible";
+            title3.style.color = "yellow";
+            title3.innerHTML = "nilfgaard";
+            eagle.style.display = "none";
+            unicorn.style.display = "none";
+            title1.style.visibility = "hidden";
+            title2.style.visibility = "hidden";
+        })
+    }
+}
+
 unicorn.addEventListener('mousemove', function() {
     body.style.background = "yellow";
     title1.style.visibility = "visible";
@@ -56,6 +100,7 @@ sun.addEventListener('mousemove', function() {
     eagle.style.visibility = "hidden";
     unicorn.style.visibility = "hidden";
     title1.style.visibility = "hidden";
+    title2.style.visibility = "hidden";
 })
 
 sun.addEventListener('mouseout', function() {
@@ -63,5 +108,9 @@ sun.addEventListener('mouseout', function() {
     title3.style.visibility = "hidden";
     eagle.style.visibility = "visible";
     unicorn.style.visibility = "visible";
-    title1.style.visibility = "hidden"; //execute order 66
+    title1.style.visibility = "hidden";
+    title2.style.visibility = "visible";
+    title3.style.visibility = "visible";
 })
+
+otherWise();
