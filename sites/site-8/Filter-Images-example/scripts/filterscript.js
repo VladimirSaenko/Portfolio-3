049a420658747bbd573df1fa46a476d2c530667a
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-21 11:45:15 +0200

    add site-8
    add site-"Filter-Images-example"

---
+++ sites/site-8/Filter-Images-example/scripts/filterscript.js
@@ -0,0 +1,23 @@
+
+"use strict"
+
+const liItem = document.querySelectorAll('.filter-menu li');
+const imgItem = document.querySelectorAll('.product .itemBox');
+
+liItem.forEach(li => {
+    li.onclick = () => {
+        liItem.forEach(other => {
+            other.className = "";
+        })
+        li.className = "active";
+        const value = li.textContent;
+        imgItem.forEach(img => {
+            img.classList.add("delete");
+            img.classList.remove("active");
+            if(img.getAttribute('data-item') == value.toLowerCase() || value == "All") {
+                img.classList.add("active");
+                img.classList.remove("delete");
+            }
+        })
+    }
+})
