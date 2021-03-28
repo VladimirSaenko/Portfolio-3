b94481d7f51c312f898c57d53f12fd9999e3ecf2
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-28 10:48:29 +0300

    add site-9
    add site-"3D Robot Model"

---
+++ sites/site-9/3D-Robot-Model/mainscript.js
@@ -0,0 +1,51 @@
+
+"use string"
+
+const modelViewer = document.getElementById('model');
+const animBtn = document.querySelectorAll('.btn-container button');
+
+animBtn.forEach(btns => {
+    btns.onclick = () => {
+        modelViewer.animationName = btns.innerHTML;
+    }
+})
+
+// hidenBtn.onclick = () => {
+//     document.querySelector('.btn-container').classList.toggle('hide');
+//     if(hidenBtn.innerHTML == 'Hide') {
+//         hidenBtn.innerHTML = 'Show';
+//     }
+//     else {
+//         hidenBtn.innerHTML = 'Hide';
+//     }
+// }
+
+// внизу два способа
+
+// function Anim(e) {
+//     modelViewer.animationName = e.innerHTML;
+// }
+
+// function Idle() {
+//     modelViewer.animationName = 'Idle';
+// }
+
+// function Dance() {
+//     modelViewer.animationName = 'Dance';
+// }
+
+// function Running() {
+//     modelViewer.animationName = 'Running';
+// }
+
+// function Death() {
+//     modelViewer.animationName = 'Death';
+// }
+
+// function Jump() {
+//     modelViewer.animationName = 'Jump';
+// }
+
+// function WalkingPutin() {
+//     modelViewer.animationName = 'Walking';
+// }
