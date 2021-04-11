e268e92334d41f17b37ca2dfbd3d263895f9c001
Vladimir Saenko <vova.saenko1@gmail.com>
2021-04-11 11:34:47 +0300

    add popup && add new media in site-11
    add popup && add new media in site-"Preloader-example-site"
    is not real ads

--- sites/site-11/Preloader-example/preloader.js
+++ sites/site-11/Preloader-example/preloader.js
@@ -2,6 +2,8 @@
 "use strict"
 
 const stars = document.querySelectorAll("#star");
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
 
 stars.forEach(star => {
     let duration = Math.random() * (0.4) + 1.3;
@@ -12,4 +14,17 @@ stars.forEach(star => {
 window.addEventListener('load', function() {
     const preloader = document.querySelector(".preloader");
     preloader.classList.add("preload-finish");
-})
+
+    setTimeout(()=> {
+        popup.style.display = "block";
+    }, 2000);
+
+    setInterval(()=> {
+        popup.style.display = "block";
+    }, 17500);
+
+});
+
+close.addEventListener('click', function() {
+    popup.style.display = "none";
+});
