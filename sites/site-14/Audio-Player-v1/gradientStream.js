3d1e2a6d326a3103f3c8edf4317d172ce1914630
Vladimir Saenko <vova.saenko1@gmail.com>
2021-04-11 20:29:27 +0300

    update site-14
    update site-"Audio Player v1"
    add img,audio, tittle &&  js script

---
+++ sites/site-14/Audio-Player-v1/gradientStream.js
@@ -0,0 +1,20 @@
+
+"use strict"
+
+let body = document.body;
+let randomdeg = getRandomInt(0,360);
+
+function letsGoParty() {
+    setInterval(function() {
+        body.style.background = 'linear-gradient(' + randomdeg + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+        ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+        ',' + getRandomInt(0,255) + ')' + ')';
+        body.style.transition = "10s";
+    }, 1000)
+}
+
+letsGoParty();
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
