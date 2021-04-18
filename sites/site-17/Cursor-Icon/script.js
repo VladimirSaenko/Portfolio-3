86f310205cd9c6b65b3c4b7701d3eb2f967fc1f2
Vladimir Saenko <vova.saenko1@gmail.com>
2021-04-18 13:40:38 +0300

    add site-17
    add site-"Cursor Icons"

---
+++ sites/site-17/Cursor-Icon/script.js
@@ -0,0 +1,25 @@
+
+"use strict"
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove',(e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
+
+function changeCursor() {
+    cursor.style.width = 60 + 'px';
+    cursor.style.height = 60 + 'px';
+    cursor.style.border = '3.5px solid #2196f3';
+}
+
+function defaultCursor() {
+    cursor.style.width = 0 + 'px';
+    cursor.style.height = 0 + 'px';
+}
+
+const items = document.querySelectorAll('.changeCursor');
+items.forEach(i => {
+    i.onmousemove = changeCursor;
+    i.onmouseout = defaultCursor;
+});
