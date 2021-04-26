d3cd7c5f1401091429322cc203275a66eedc7c67
Vladimir Saenko <vova.saenko1@gmail.com>
2021-04-26 14:51:37 +0300

    add site-18
    add site-"VideoOnHover"

---
+++ sites/site-18/VideoOnHover/script.js
@@ -0,0 +1,13 @@
+
+"use strict"
+
+const clip = document.querySelectorAll('video');
+
+for(let i = 0; i < clip.length; i++) {
+    clip[i].addEventListener('mouseenter', function(e) {
+        clip[i].play();
+    });
+    clip[i].addEventListener('mouseout', function(e) {
+        clip[i].pause();
+    });
+}
