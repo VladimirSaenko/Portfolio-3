837cad62999e73e1a8c12cd27d23631d29aeb782
Vladimir Saenko <vova.saenko1@gmail.com>
2021-04-27 21:40:05 +0300

    Updating in site-"Grogu Mandalore Toggle "
    Update script and add git link supporter(Спасибо,Сань) and update icon

--- sites/site-13/Grogu-Mandalorian-Toggle/script.js
+++ sites/site-13/Grogu-Mandalorian-Toggle/script.js
@@ -1,13 +1,24 @@
 
 "use strict"
 
-let mandalorian = document.getElementById("mando");
-// let yoda = document.getElementById("yoda");
+let mando = document.getElementById("mando");
 let container = document.getElementById("container");
 let toggle = document.getElementById("slider");
+let gitSupp = document.getElementById("gitSupp");
+let isActive = false;
 
 function activateMando() {
-    mandalorian.style.visibility = 'visible';
-    container.style.background = '#121011';
-    toggle.style.backgroundImage = "linear-gradient(#000000,#5a5252,#585552, #6d6565, #000000)";
+    if(!isActive){
+        mando.style.visibility = 'visible';
+        gitSupp.style.visibility = 'visible';
+        container.style.background = '#121011';
+        toggle.style.backgroundImage = "linear-gradient(#000000,#5a5252,#585552, #6d6565, #000000)";
+    }
+    else if(isActive){
+        mando.style.visibility = 'hidden';
+        gitSupp.style.visibility = 'hidden';
+        container.style.background = '#5ec4e0';
+        toggle.style.backgroundImage = "linear-gradient( #aefaa7, #00ff22, #1eff00, #33ff00, #aefda7 )";
+    }
+    isActive = !isActive;  
 }
