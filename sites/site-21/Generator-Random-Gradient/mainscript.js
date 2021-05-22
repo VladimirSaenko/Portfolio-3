334918e7c4b37c2a5eda6525f07762f5f8e5b024
Vladimir Saenko <vova.saenko1@gmail.com>
2021-05-22 14:35:59 +0300

    update js script in site-21
    update js script in site-"Generator Random Gradient"

--- sites/site-21/Generator-Random-Gradient/mainscript.js
+++ sites/site-21/Generator-Random-Gradient/mainscript.js
@@ -4,17 +4,33 @@
 let gradientText = document.getElementById('gradient-text');
 let box = document.getElementById("box");
 let body = document.body;
-
-box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+let directions = [
+    "right",
+    "left",
+    "top",
+    "bottom"
+];
+box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0,4)]}` + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
 ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
 ',' + getRandomInt(0,255) + ')' + ')';
+// Новый способ
 body.style.background = box.style.background;
 gradientText.value = box.style.background;
+// box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+// ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+// ',' + getRandomInt(0,255) + ')' + ')';
+// Старый способ
+
 
 function getRandomGradient() {
-    box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+    // box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+    // ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
+    // ',' + getRandomInt(0,255) + ')' + ')';
+    // Старый способ
+    box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0,4)]}` + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
     ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
     ',' + getRandomInt(0,255) + ')' + ')';
+    // Новый способ, оба отлично работают
     body.style.background = box.style.background;
     gradientText.value = box.style.background;
 }
