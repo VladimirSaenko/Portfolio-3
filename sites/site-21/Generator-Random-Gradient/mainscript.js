1b5371068f3c6d7f9ef6bb8f5b61b603ab9135bc
Vladimir Saenko <vova.saenko1@gmail.com>
2021-05-29 14:16:47 +0300

    upd js script in site-21
    upd js script in site-"Generator Random Gradient"

--- sites/site-21/Generator-Random-Gradient/mainscript.js
+++ sites/site-21/Generator-Random-Gradient/mainscript.js
@@ -13,24 +13,13 @@ let directions = [
 box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0,4)]}` + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
 ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
 ',' + getRandomInt(0,255) + ')' + ')';
-// Новый способ
 body.style.background = box.style.background;
 gradientText.value = box.style.background;
-// box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
-// ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
-// ',' + getRandomInt(0,255) + ')' + ')';
-// Старый способ
-
 
 function getRandomGradient() {
-    // box.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
-    // ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
-    // ',' + getRandomInt(0,255) + ')' + ')';
-    // Старый способ
     box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0,4)]}` + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
     ',' + getRandomInt(0,255) + ')' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
     ',' + getRandomInt(0,255) + ')' + ')';
-    // Новый способ, оба отлично работают
     body.style.background = box.style.background;
     gradientText.value = box.style.background;
 }
