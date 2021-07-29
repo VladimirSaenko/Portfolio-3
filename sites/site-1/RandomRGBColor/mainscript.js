b15e821b08efa405ece849d7578a1fbc7f487a97
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-07-29 15:00:25 +0300

    Upd script in site-1

--- sites/site-1/RandomRGBColor/mainscript.js
+++ sites/site-1/RandomRGBColor/mainscript.js
@@ -1,5 +1,5 @@
 
-"use strict"
+"use strict";
 
 let box = document.getElementById("mainBox");
 let inputRgb = document.getElementById("mainInput");
@@ -18,7 +18,6 @@ userColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,
 userColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
 
 function getRandomRGBColor() {
-
     box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
     inputRgb.value = box.style.background + ';';
     inputRgb.style.color = box.style.background;
@@ -26,9 +25,7 @@ function getRandomRGBColor() {
     getRandomRGBColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
     userColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
     userColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
-
     ifEqualyColors();
-
     if(userColorsList.style.display == "flex" && inputRgb.style.display == "none" && copyBtn.style.display == "none") {
         userColorsList.style.display = "none";
         InputUserColor1.style.display = "none";
@@ -40,14 +37,11 @@ function getRandomRGBColor() {
 }
 
 function ifEqualyColors() {
-
     if(getRandomRGBColorBtn.style.background == box.style.background ) {
-
         box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
         inputRgb.value = box.style.background + ';';
         inputRgb.style.color = box.style.background;
         getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
-
         if(userColorBtn.style.background == getRandomRGBColorBtn.style.background || userColorBtn.style.background == getRandomRGBColorBtn.style.color ||
             userColorBtn.style.background == inputRgb.style.color || userColorBtn.style.background == userColorBtn.style.color ||
             userColorBtn.style.color == getRandomRGBColorBtn.style.background || userColorBtn.style.color == getRandomRGBColorBtn.style.background ||
@@ -56,16 +50,13 @@ function ifEqualyColors() {
                 userColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
         }
     }
-
     if(getRandomRGBColorBtn.style.color == getRandomRGBColorBtn.style.background || getRandomRGBColorBtn.style.color == inputRgb.style.color ||
        getRandomRGBColorBtn.style.color == box.style.background) {
-
         box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
         inputRgb.value = box.style.background + ';';
         inputRgb.style.color = box.style.background;
         getRandomRGBColorBtn.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
         getRandomRGBColorBtn.style.color = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
-
         if(userColorBtn.style.background == getRandomRGBColorBtn.style.background || userColorBtn.style.background == getRandomRGBColorBtn.style.color ||
             userColorBtn.style.background == inputRgb.style.color || userColorBtn.style.background == userColorBtn.style.color ||
             userColorBtn.style.color == getRandomRGBColorBtn.style.background || userColorBtn.style.color == getRandomRGBColorBtn.style.background ||
@@ -79,7 +70,6 @@ function ifEqualyColors() {
 function activateInfinityStreamColor() {
     setInterval(
         function getRandomRGBColor2() {
-
             box.style.background = 'rgb(' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ', ' + getRandomInt(0,255) + ')';
             inputRgb.style.display = "none";
             getRandomRGBColorBtn.style.display = "none";
@@ -88,7 +78,6 @@ function activateInfinityStreamColor() {
             userColorBtn.style.display = "none";
             let body = document.body;
             body.style.cursor = "none";
-
             if(userColorsList.style.display == "flex") {
                 userColorsList.style.display = "none";
                 InputUserColor1.style.display = "none";
@@ -107,7 +96,6 @@ function activateUserRGBColor() {
     inputRgb.style.display = "none";
     copyBtn.style.display = "none";
     box.style.background = 'rgb(' + 0 + ', ' +  0  + ', ' +  0  + ')';
-
     if(InputUserColor1.value > 0 || InputUserColor2.value > 0 || InputUserColor3.value > 0) {
         box.style.background = 'rgb(' + InputUserColor1.value + ', ' + InputUserColor2.value + ', ' + InputUserColor3.value + ')';
     }
@@ -118,7 +106,6 @@ function getUserRGBColor() {
 }
 
 function ifMoreOrLessValueUserColor() {
-
     if(InputUserColor1.value < 0 || InputUserColor1.value > 255 ) {
         InputUserColor1.style.color = "red";
 
@@ -129,10 +116,8 @@ function ifMoreOrLessValueUserColor() {
             InputUserColor3.style.color = "red";
     }
     }
-
     if(InputUserColor2.value < 0 || InputUserColor2.value > 255 ) {
         InputUserColor2.style.color = "red";
-
         if(InputUserColor2.value < 0 || InputUserColor2.value > 255 ) {
             InputUserColor2.style.color = "red";
         }
@@ -140,32 +125,24 @@ function ifMoreOrLessValueUserColor() {
             InputUserColor3.style.color = "red";
         }
     }
-
     if(InputUserColor3.value < 0 || InputUserColor3.value > 255 ) {
         InputUserColor3.style.color = "red";
-
         if(InputUserColor2.value < 0 || InputUserColor2.value > 255 ) {
             InputUserColor2.style.color = "red";
         }
-
         if(InputUserColor3.value < 0 || InputUserColor3.value > 255 ) {
             InputUserColor3.style.color = "red";
         }
-
     }
-    
     if(InputUserColor1.value > 0 && InputUserColor1.value <= 255) {
         InputUserColor1.style.color = "black";
     }
-
     if(InputUserColor2.value > 0 && InputUserColor2.value <= 255) {
         InputUserColor2.style.color = "black";
     }
-
     if(InputUserColor3.value > 0 && InputUserColor3.value <= 255) {
         InputUserColor3.style.color = "black";
     }
-
 }
 
 function copyRGBColor() {
