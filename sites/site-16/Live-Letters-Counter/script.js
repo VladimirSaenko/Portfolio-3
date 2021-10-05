ace64aa2c69de9114ce9a8ece6fc9285d4fefca3
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-05 23:36:52 +0300

    add focus in site-16
    add focus in site-16

--- sites/site-16/Live-Letters-Counter/script.js
+++ sites/site-16/Live-Letters-Counter/script.js
@@ -4,6 +4,8 @@
 const textarea = document.querySelector("textarea");
 const count = document.querySelector(".count");
 
+textarea.focus();
+
 function countLetters() {
     const textWithoutSpace = textarea.value;
     const textLength = textWithoutSpace.length;
