41c46b2c4aa90d2feb578e55a8dfb21c7cde4850
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-20 16:57:12 +0300

    Update in site-30
    Update in site-"PS Trophy Creator"

--- sites/site-30/PS-Trophy-Creator-site/mainscript.js
+++ sites/site-30/PS-Trophy-Creator-site/mainscript.js
@@ -7,68 +7,54 @@ let trophytypeInput = document.getElementById("trophytype");
 let trophyImg = document.getElementById("trophyImg");
 let trophyName = document.getElementById("trophyName");
 let mainbutton = document.getElementById("mainbutton");
-let isActive = false;
-
 trophynameInput.focus();
 // trophytypeInput.focus();
 
 mainbutton.addEventListener("click", function() {
-    if(isActive == false) {
-        setTimeout(function() {
-            trophyIcon.style.display = "flex";
-            trophyIcon.style.animation = "uprise 0.5s forwards";
-            trophyName.innerText = trophynameInput.value;
-            if(trophynameInput.value == '') {
-                trophyName.innerText = "empty";
-            }
-            getTrophyInType();
-        }, 500);
+    trophyName.innerText = trophynameInput.value;
+    getTrophyInType();
+    trophyIcon.classList.add('active');
+
+    if(trophynameInput.value == '') {
+        trophyName.innerText = "Empty";
     }
-    if(!isActive) {
-        trophyIcon.style.display = "none";
-        trophyIcon.style.animation = "returnUprise 0.5s forwards";
-        trophyName.innerText = "";
-        trophyImg.src = "";
-        setTimeout(function() {
-            trophyIcon.style.display = "flex";
-            trophyIcon.style.animation = "uprise 0.5s forwards";
-            trophyName.innerText = trophynameInput.value;
-            if(trophynameInput.value == '') {
-                trophyName.innerText = "empty";
-            }
-            getTrophyInType();
-        }, 500);
-    }
-    isActive = !isActive;
 })
 
 function getTrophyInType() {
     if(trophytypeInput.value == "") {
-        let random = getRandomInt(1,4);
-        if(random == 1) {
+        let random = getRandomInt(1,5);
+
+        if(random == 1 || random == 5) {
             trophyImg.src = "images/bronze-trophy.png";
         }
+
         if(random == 2) {
             trophyImg.src = "images/silver-trophy.png";
         }
+
         if(random == 3) {
             trophyImg.src = "images/gold-trophy.png";
         }
+        
         if(random == 4) {
             trophyImg.src = "images/platinum-trophy.png";
         }
     }
+
     else {
-        if(trophytypeInput.value == "Бронза") {
+        if (trophytypeInput.value == "Бронза") {
             trophyImg.src = "images/bronze-trophy.png";
         }
-        if(trophytypeInput.value == "Серебро") {
+
+        if (trophytypeInput.value == "Серебро") {
             trophyImg.src = "images/silver-trophy.png";
         }
-        if(trophytypeInput.value == "Золото") {
+
+        if (trophytypeInput.value == "Золото") {
             trophyImg.src = "images/gold-trophy.png";
         }
-        if(trophytypeInput.value == "Платина") {
+
+        if (trophytypeInput.value == "Платина") {
             trophyImg.src = "images/platinum-trophy.png";
         }
     }
