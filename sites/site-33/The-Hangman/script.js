313efbd4bcb2deddd9977137af33fd7181a1f7b2
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-11-01 00:07:17 +0200

    fix bug in site-33
    fix bug in site-"The-Hangman game"

--- sites/site-33/The-Hangman/script.js
+++ sites/site-33/The-Hangman/script.js
@@ -48,7 +48,7 @@ function displayWord() {
 	wordEl.innerHTML = `
 	${selectedWord.split('').map(letter => `
 	<span class="letter">
-	${correctLetters.includes(letter) ? letter : ''}<span/>`
+	${correctLetters.includes(letter) ? letter : ''}</span>`
 	).join('')}`;
 
 	// Add Congratulations
@@ -58,7 +58,7 @@ function displayWord() {
 	// console.log(innerWord);
 	const innerWord = wordEl.innerText.replace(/\n/g, '');
 	if(innerWord === selectedWord) {
-		finalMessage.innerText = `Congratulations!, the word: ${selectedWord}`;
+		finalMessage.innerText = 'Congratulations!';
 		finalMessageRevealWord.innerText = '';
 		popup.style.display = 'flex';
 		playable = false;
@@ -68,7 +68,7 @@ function displayWord() {
 // Update the wrong letters
 function updateWrongLettersEl() {
 	wrongLettersEl.innerHTML = `
-	${wrongLetters.length > 0 ? '<p>Wrong<p/>' : ''}
+	${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
 	${wrongLetters.map(letter => `<span>${letter}</span>`)}
 	`;
 
