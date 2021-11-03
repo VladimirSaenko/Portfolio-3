56123e3bc1d6ee39023f8626f5d091fea091a889
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-11-03 23:11:18 +0200

    add new hats in site-26
    add new hats in site-"Among Us Space Scene"

--- sites/site-26/AmoGUS/scripts/mainscript.js
+++ sites/site-26/AmoGUS/scripts/mainscript.js
@@ -16,7 +16,7 @@ color7.style.fill = 'rgb(225, 225, 225)';
 let impostortext = document.getElementById("impostertext");
 let hat = document.getElementById("hat"); //шляпа
 let randomColor = getRandomInt(0,22);
-let randomHat = getRandomInt(1,30);
+let randomHat = getRandomInt(1,35);
 hat.src = `hats/${randomHat}.png`;
 const colors = [
     'Red',
@@ -946,7 +946,7 @@ getHat();
 
 function getHat() {
     setInterval(function() {
-        randomHat = getRandomInt(1,30);
+        randomHat = getRandomInt(1,35);
         hat.src = `hats/${randomHat}.png`;
     }, 5000);
 }
