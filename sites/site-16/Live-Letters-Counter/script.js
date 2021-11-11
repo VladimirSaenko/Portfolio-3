1e480f13b7550a004e0a04061cc20097379df63d
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-11-11 15:49:27 +0200

    Update script js in site-16
    Update script js in site-"Live Letters Counter"

--- sites/site-16/Live-Letters-Counter/script.js
+++ sites/site-16/Live-Letters-Counter/script.js
@@ -7,6 +7,7 @@ const count = document.querySelector(".count");
 textarea.focus();
 
 function countLetters() {
+    // const textWithoutSpace = textarea.value.replace(/\s/g, '');
     const textWithoutSpace = textarea.value;
     const textLength = textWithoutSpace.length;
     count.innerHTML = textLength;
