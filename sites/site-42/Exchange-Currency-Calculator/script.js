b6946cbd0a181d5c3ac31c24dd22df64fe0ffb8f
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2021-12-05 12:08:21 +0200

    add new sites 41, 42
    Notes, Exchange Currency Calculator

---
+++ sites/site-42/Exchange-Currency-Calculator/script.js
@@ -0,0 +1,27 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+async function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    const res = await fetch('https://open.exchangerate-api.com/v6/latest');
+    // console.log('res', res);
+    const data = await res.json();
+    // console.log('data', data);
+    const rate = data.rates[currency_two] / data.rates[currency_one];
+    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+    amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+}
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
+
+// swap.addEventListener('')
