823f21aca48c5946a447db02af1f149196fbdda9
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2021-12-09 23:29:34 +0200

    Update script in site-42
    Update script in site-"Exchange Currency Calculator"

--- sites/site-42/Exchange-Currency-Calculator/script.js
+++ sites/site-42/Exchange-Currency-Calculator/script.js
@@ -24,16 +24,23 @@ amountEl_two.addEventListener('input', calculate);
 
 calculate();
 
-swap.addEventListener('click', async () => {
-    const currency_one = currencyEl_two.value;
-    const currency_two = currencyEl_one.value;
-    const res = await fetch('https://open.exchangerate-api.com/v6/latest');
-    // console.log('res', res);
-    const data = await res.json();
-    // console.log('data', data);
-    const rate = data.rates[currency_two] / data.rates[currency_one];
-    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-    amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
-    currencyEl_one.value = currency_one;
-    currencyEl_two.value = currency_two
-})
+// swap.addEventListener('click', async () => {
+//     const currency_one = currencyEl_two.value;
+//     const currency_two = currencyEl_one.value;
+//     const res = await fetch('https://open.exchangerate-api.com/v6/latest');
+//     // console.log('res', res);
+//     const data = await res.json();
+//     // console.log('data', data);
+//     const rate = data.rates[currency_two] / data.rates[currency_one];
+//     rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+//     amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+//     currencyEl_one.value = currency_one;
+//     currencyEl_two.value = currency_two
+// })
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+  });
