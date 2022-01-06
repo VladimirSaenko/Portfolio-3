aa5e8ac6461e8921a1c99a66b79f9a9e4d4b0199
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-06 17:33:08 +0200

    Update script in site-16
    Update script in site-"Live Letters Counter"

--- sites/site-16/Live-Letters-Counter/script.js
+++ sites/site-16/Live-Letters-Counter/script.js
@@ -7,8 +7,7 @@ const count = document.querySelector(".count");
 textarea.focus();
 
 function countLetters() {
-    // const textWithoutSpace = textarea.value.replace(/\s/g, '');
-    const textWithoutSpace = textarea.value;
+    const textWithoutSpace = textarea.value.replace(/\s/g, '');
     const textLength = textWithoutSpace.length;
     count.innerHTML = textLength;
 }
