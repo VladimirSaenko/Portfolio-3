a29ce9259f5bb902e32e5c89344eec9ffc258c3d
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-12 19:17:09 +0200

    add new 2 contries in site-5
    add new 2 contries in site-"Witcher SVG Icons"

--- sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
+++ sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
@@ -7,34 +7,32 @@ let sun = document.getElementById("nilfgaard");
 let eagle = document.getElementById("redania");
 let drakkar = document.getElementById("drakkar");
 let helmet = document.getElementById("helmet");
+let axes = document.getElementById("axes");
+let fish = document.getElementById("fish");
 let title1 = document.getElementById("title1");
 let title2 = document.getElementById("title2");
 let title3 = document.getElementById("title3");
 let title4 = document.getElementById("title4");
 let title5 = document.getElementById("title5");
+let title6 = document.getElementById("title6");
+let title7 = document.getElementById("title7");
 
 unicorn.addEventListener('mousemove', function() {
     body.style.background = "yellow";
     title1.style.visibility = "visible";
     title1.innerHTML = "kaedwen";
     title1.style.color = "black";
-    // sun.style.visibility = "hidden";
-    // eagle.style.visibility = "hidden";
-    // drakkar.style.visibility = "hidden";
-    // helmet.style.visibility = "hidden";
     title2.style.visibility = "hidden";
     title3.style.visibility = "hidden";
     title4.style.visibility = "hidden";
     title5.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
 })
 
 unicorn.addEventListener('mouseout', function() {
     body.style.background = "#06181f";
     title1.style.visibility = "hidden";
-    // sun.style.visibility = "visible";
-    // eagle.style.visibility = "visible";
-    // drakkar.style.visibility = "visible";
-    // helmet.style.visibility = "visible";
 })
 
 eagle.addEventListener("mousemove",function() {
@@ -42,22 +40,18 @@ eagle.addEventListener("mousemove",function() {
     title2.style.visibility = "visible";
     title2.style.color = "white";
     title2.innerHTML = "redania";
-    // sun.style.visibility = "hidden";
-    // unicorn.style.visibility = "hidden";
-    // drakkar.style.visibility = "hidden";
     title1.style.visibility = "hidden";
     title3.style.visibility = "hidden";
     title4.style.visibility = "hidden";
     title5.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
 })
 
 eagle.addEventListener('mouseout',function() {
     body.style.background = "#06181f";
     sun.style.visibility = "visible";
     title2.style.visibility = "hidden";
-    // unicorn.style.visibility = "visible";
-    // drakkar.style.visibility = "visible";
-    // helmet.style.visibility = "visible";
 })
 
 sun.addEventListener('mousemove', function() {
@@ -65,22 +59,17 @@ sun.addEventListener('mousemove', function() {
     title3.style.visibility = "visible";
     title3.style.color = "yellow";
     title3.innerHTML = "nilfgaard";
-    // eagle.style.visibility = "hidden";
-    // unicorn.style.visibility = "hidden";
-    // drakkar.style.visibility = "hidden";
     title1.style.visibility = "hidden";
     title2.style.visibility = "hidden";
     title4.style.visibility = "hidden";
     title5.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
 })
 
 sun.addEventListener('mouseout', function() {
     body.style.background = "#06181f";
     title3.style.visibility = "hidden";
-    // eagle.style.visibility = "visible";
-    // unicorn.style.visibility = "visible";
-    // drakkar.style.visibility = "visible";
-    // helmet.style.visibility = "visible";
 })
 
 drakkar.addEventListener('mousemove', function() {
@@ -88,23 +77,17 @@ drakkar.addEventListener('mousemove', function() {
     title4.style.visibility = "visible";
     title4.innerHTML = "skellige";
     title4.style.color = "white";
-    // sun.style.visibility = "hidden";
-    // eagle.style.visibility = "hidden";
-    // unicorn.style.visibility = "hidden";
-    // helmet.style.visibility = "hidden";
     title1.style.visibility = "hidden";
     title2.style.visibility = "hidden";
     title3.style.visibility = "hidden";
     title5.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
 })
 
 drakkar.addEventListener('mouseout', function() {
     body.style.background = "#06181f";
     title4.style.visibility = "hidden";
-    // eagle.style.visibility = "visible";
-    // unicorn.style.visibility = "visible";
-    // sun.style.visibility = "visible";
-    // helmet.style.visibility = "visible";
 })
 
 helmet.addEventListener('mousemove', function() {
@@ -112,21 +95,51 @@ helmet.addEventListener('mousemove', function() {
     title5.style.visibility = "visible";
     title5.innerHTML = "Toussaint";
     title5.style.color = "yellow";
-    // sun.style.visibility = "hidden";
-    // eagle.style.visibility = "hidden";
-    // unicorn.style.visibility = "hidden";
-    // drakkar.style.visibility = "hidden";
     title1.style.visibility = "hidden";
     title2.style.visibility = "hidden";
     title3.style.visibility = "hidden";
     title4.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
 })
 
 helmet.addEventListener('mouseout', function() {
     body.style.background = "#06181f";
     title5.style.visibility = "hidden";
-    // eagle.style.visibility = "visible";
-    // unicorn.style.visibility = "visible";
-    // sun.style.visibility = "visible";
-    // drakkar.style.visibility = "visible";
+})
+
+axes.addEventListener('mousemove', function() {
+    body.style.background = "black";
+    title6.style.visibility = 'visible';
+    title6.innerText = "Mahakam";
+    title5.style.color = "white";
+    title1.style.visibility = "hidden";
+    title2.style.visibility = "hidden";
+    title3.style.visibility = "hidden";
+    title4.style.visibility = "hidden";
+    title5.style.visibility = "hidden";
+    title7.style.visibility = "hidden";
+})
+
+axes.addEventListener('mouseleave', function() {
+    body.style.background = "#06181f";
+    title6.style.visibility = 'hidden';
+})
+
+fish.addEventListener('mousemove', function() {
+    body.style.background = "yellow";
+    title7.style.visibility = 'visible';
+    title7.innerText = "Kerack";
+    title7.style.color = "black";
+    title1.style.visibility = "hidden";
+    title2.style.visibility = "hidden";
+    title3.style.visibility = "hidden";
+    title4.style.visibility = "hidden";
+    title5.style.visibility = "hidden";
+    title6.style.visibility = "hidden";
+})
+
+fish.addEventListener("mouseleave", function() {
+    body.style.background = "#06181f";
+    title7.style.visibility = 'hidden';
 })
