3cdafa94403278c5fe95c21fc96583c7f0c414f8
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-15 13:03:23 +0200

    Update questions in site-38
    Update questions in site-""Quiz Example"

--- sites/site-38/Quiz-Example/scripts/quiz.js
+++ sites/site-38/Quiz-Example/scripts/quiz.js
@@ -1,178 +1,178 @@
 const questions = [
 	{
-			question: "How many days makes a week ?",
-			optionA: "10 days",
-			optionB: "14 days",
-			optionC: "5 days",
-			optionD: "7 days",
-			correctOption: "optionD"
+		question: "How many days makes a week ?",
+		optionA: "10 days",
+		optionB: "14 days",
+		optionC: "5 days",
+		optionD: "7 days",
+		correctOption: "optionD"
 	},{
-			question: "How many players are allowed on a soccer pitch ?",
-			optionA: "10 players",
-			optionB: "11 players",
-			optionC: "9 players",
-			optionD: "12 players",
-			correctOption: "optionB"
+		question: "How many players are allowed on a soccer pitch ?",
+		optionA: "10 players",
+		optionB: "11 players",
+		optionC: "9 players",
+		optionD: "12 players",
+		correctOption: "optionB"
 	},{
-			question: "Who was the first President of USA ?",
-			optionA: "Donald Trump",
-			optionB: "Barack Obama",
-			optionC: "Abraham Lincoln",
-			optionD: "George Washington",
-			correctOption: "optionD"
+		question: "Who was the first President of USA ?",
+		optionA: "Donald Trump",
+		optionB: "Barack Obama",
+		optionC: "Abraham Lincoln",
+		optionD: "George Washington",
+		correctOption: "optionD"
 	},{
-			question: "30 days has ______ ?",
-			optionA: "January",
-			optionB: "December",
-			optionC: "June",
-			optionD: "August",
-			correctOption: "optionC"
+		question: "30 days has ______ ?",
+		optionA: "January",
+		optionB: "December",
+		optionC: "June",
+		optionD: "August",
+		correctOption: "optionC"
 	},{
-			question: "How many hours can be found in a day ?",
-			optionA: "30 hours",
-			optionB: "38 hours",
-			optionC: "48 hours",
-			optionD: "24 hours",
-			correctOption: "optionD"
+		question: "How many hours can be found in a day ?",
+		optionA: "30 hours",
+		optionB: "38 hours",
+		optionC: "48 hours",
+		optionD: "24 hours",
+		correctOption: "optionD"
 	},{
-			question: "Which is the longest river in the world ?",
-			optionA: "River Nile",
-			optionB: "Long River",
-			optionC: "River Dnieper",
-			optionD: "Lake Chad",
-			correctOption: "optionA"
+		question: "Which is the longest river in the world ?",
+		optionA: "River Nile",
+		optionB: "Long River",
+		optionC: "River Dnieper",
+		optionD: "Lake Chad",
+		correctOption: "optionA"
 	},{
-			question: "_____ is the hottest Continent on Earth ?",
-			optionA: "Oceania",
-			optionB: "Antarctica",
-			optionC: "Africa",
-			optionD: "North America",
-			correctOption: "optionC"
+		question: "_____ is the hottest Continent on Earth ?",
+		optionA: "Oceania",
+		optionB: "Antarctica",
+		optionC: "Africa",
+		optionD: "North America",
+		correctOption: "optionC"
 	},{
-			question: "Which country is the largest in the world ?",
-			optionA: "Russia",
-			optionB: "Canada",
-			optionC: "China",
-			optionD: "Egypt",
-			correctOption: "optionA"
+		question: "Which country is the largest in the world ?",
+		optionA: "Russia",
+		optionB: "Canada",
+		optionC: "China",
+		optionD: "Egypt",
+		correctOption: "optionA"
 	},{
-			question: "Which of these numbers is an odd number ?",
-			optionA: "Ten",
-			optionB: "Twelve",
-			optionC: "Eight",
-			optionD: "Eleven",
-			correctOption: "optionD"
+		question: "Which of these numbers is an odd number ?",
+		optionA: "Ten",
+		optionB: "Twelve",
+		optionC: "Eight",
+		optionD: "Eleven",
+		correctOption: "optionD"
 	},{
-			question: `"You Can't see me" is a popular saying by...`,
-			optionA: "Eminem",
-			optionB: "Bill Gates",
-			optionC: "Chris Brown",
-			optionD: "John Cena",
-			correctOption: "optionD"
+		question: `"You Can't see me" is a popular saying by...`,
+		optionA: "Eminem",
+		optionB: "Bill Gates",
+		optionC: "Chris Brown",
+		optionD: "John Cena",
+		correctOption: "optionD"
 	},{
-			question: "Where is the world tallest building located ?",
-			optionA: "Africa",
-			optionB: "California",
-			optionC: "Dubai",
-			optionD: "Italy",
-			correctOption: "optionC"
+		question: "Where is the world tallest building located ?",
+		optionA: "Albany",
+		optionB: "Canada",
+		optionC: "Dubai",
+		optionD: "Italy",
+		correctOption: "optionC"
 	},{
-			question: "The longest river in the Ukraine is ?",
-			optionA: "River Dnieper",
-			optionB: "River Nile",
-			optionC: "River Southern Bug",
-			optionD: "River Twitter",
-			correctOption: "optionA"
+		question: "The longest river in the Ukraine is ?",
+		optionA: "River Dnieper",
+		optionB: "River Nile",
+		optionC: "River Southern Bug",
+		optionD: "River Twitter",
+		correctOption: "optionA"
 	},{
-			question: "How many permanent teeth does a dog have ?",
-			optionA: "38",
-			optionB: "42",
-			optionC: "40",
-			optionD: "36",
-			correctOption: "optionB"
+		question: "How many permanent teeth does a dog have ?",
+		optionA: "38",
+		optionB: "42",
+		optionC: "40",
+		optionD: "36",
+		correctOption: "optionB"
 	},{
-			question: "Which national team won the football World cup in 2018 ?",
-			optionA: "England",
-			optionB: "Brazil",
-			optionC: "Germany",
-			optionD: "France",
-			correctOption: "optionD"
+		question: "Which national team won the football World cup in 2018 ?",
+		optionA: "England",
+		optionB: "Brazil",
+		optionC: "Germany",
+		optionD: "France",
+		correctOption: "optionD"
 	},{
-			question: "Which US state was Donald Trump Born ?",
-			optionA: "New York",
-			optionB: "California",
-			optionC: "New Jersey",
-			optionD: "Los Angeles",
-			correctOption: "optionA"
+		question: "Which US state was Donald Trump Born ?",
+		optionA: "New York",
+		optionB: "Denver",
+		optionC: "New Jersey",
+		optionD: "Los Angeles",
+		correctOption: "optionA"
 	},{
-			question: "How many states does USA have ?",
-			optionA: "24",
-			optionB: "30",
-			optionC: "50",
-			optionD: "37",
-			correctOption: "optionC"
+		question: "How many states does USA have ?",
+		optionA: "24",
+		optionB: "30",
+		optionC: "50",
+		optionD: "37",
+		correctOption: "optionC"
 	},{
-			question: "____ is the capital of Latvia ?",
-			optionA: "Riga",
-			optionB: "Budapesht",
-			optionC: "Paris",
-			optionD: "Roma",
-			correctOption: "optionA"
+		question: "____ is the capital of Latvia ?",
+		optionA: "Riga",
+		optionB: "Budapesht",
+		optionC: "Paris",
+		optionD: "Roma",
+		correctOption: "optionA"
 	},{
-			question: "Los Angeles is also known as ?",
-			optionA: "Angels City",
-			optionB: "Shining city",
-			optionC: "City of Angels",
-			optionD: "Lost Angels",
-			correctOption: "optionC"
+		question: "Los Angeles is also known as ?",
+		optionA: "Angels City",
+		optionB: "Shining city",
+		optionC: "City of Angels",
+		optionD: "Lost Angels",
+		correctOption: "optionC"
 	},{
-			question: "What is the capital of Germany ?",
-			optionA: "Georgia",
-			optionB: "Missouri",
-			optionC: "Oklahoma",
-			optionD: "Berlin",
-			correctOption: "optionD"
+		question: "What is the capital of Germany ?",
+		optionA: "Hannover",
+		optionB: "Moscow",
+		optionC: "Ottawa",
+		optionD: "Berlin",
+		correctOption: "optionD"
 	},{
-			question: "How many sides does an hexagon have ?",
-			optionA: "Six",
-			optionB: "Sevene",
-			optionC: "Four",
-			optionD: "Five",
-			correctOption: "optionA"
+		question: "How many sides does an hexagon have ?",
+		optionA: "Six",
+		optionB: "Sevene",
+		optionC: "Four",
+		optionD: "Five",
+		correctOption: "optionA"
 	},{
-			question: "How many planets are currently in the solar system ?",
-			optionA: "Eleven",
-			optionB: "Seven",
-			optionC: "Nine",
-			optionD: "Eight",
-			correctOption: "optionD"
+		question: "How many planets are currently in the solar system ?",
+		optionA: "Eleven",
+		optionB: "Seven",
+		optionC: "Nine",
+		optionD: "Eight",
+		correctOption: "optionD"
 	},{
-			question: "Which Planet is the hottest ?",
-			optionA: "Jupitar",
-			optionB: "Mercury",
-			optionC: "Earth",
-			optionD: "Venus",
-			correctOption: "optionB"
+		question: "Which Planet is the hottest ?",
+		optionA: "Jupitar",
+		optionB: "Mercury",
+		optionC: "Earth",
+		optionD: "Venus",
+		correctOption: "optionB"
 	},{
-			question: "Where is the smallest bone in human body located?",
-			optionA: "Toes",
-			optionB: "Ears",
-			optionC: "Fingers",
-			optionD: "Nose",
-			correctOption: "optionB"
+		question: "Where is the smallest bone in human body located?",
+		optionA: "Toes",
+		optionB: "Ears",
+		optionC: "Fingers",
+		optionD: "Nose",
+		correctOption: "optionB"
 	},{
-			question: "How many hearts does an Octopus have ?",
-			optionA: "One",
-			optionB: "Two",
-			optionC: "Three",
-			optionD: "Four",
-			correctOption: "optionC"
+		question: "How many hearts does an Octopus have ?",
+		optionA: "One",
+		optionB: "Two",
+		optionC: "Three",
+		optionD: "Four",
+		correctOption: "optionC"
 	},{
-			question: "How many teeth does an adult human have ?",
-			optionA: "28",
-			optionB: "30",
-			optionC: "32",
-			optionD: "36",
-			correctOption: "optionC"
+		question: "How many teeth does an adult human have ?",
+		optionA: "28",
+		optionB: "30",
+		optionC: "32",
+		optionD: "36",
+		correctOption: "optionC"
 	}
 ]
