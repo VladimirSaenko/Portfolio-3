015ae93123bc4606dc7d1d1b789136b61fced9bb
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-29 23:21:33 +0200

    Upd js script in site-24
    Upd js script in site-24

--- sites/site-24/RGB-IMAC2021/script.js
+++ sites/site-24/RGB-IMAC2021/script.js
@@ -1,16 +1,16 @@
 
 "use strict";
 
-let randomColor1 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0,255) + "," + getRandomInt(0,255) + ")";
-let randomColor2 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0,255) + "," + getRandomInt(0,255) + ")";
+let randomColor1 = 'rgb(' + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + ")";
+let randomColor2 = 'rgb(' + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + ")";
 let color1 = document.getElementById("color1");
 let color2 = document.getElementById("color2");
 document.documentElement.style.setProperty('--one-color', `${randomColor1}`);
 document.documentElement.style.setProperty('--second-color', `${randomColor2}`);
 
 function getRandomColors() {
-    randomColor1 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0,255) + "," + getRandomInt(0,255) + ")";
-    randomColor2 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0,255) + "," + getRandomInt(0,255) + ")";
+    randomColor1 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + ")";
+    randomColor2 = 'rgb(' + getRandomInt(0,255) + "," + getRandomInt(0, 255) + "," + getRandomInt(0, 255) + ")";
     document.documentElement.style.setProperty('--one-color', `${randomColor1}`);
     document.documentElement.style.setProperty('--second-color', `${randomColor2}`);
 }
