a72503a57abf596dfba760eb197143f5ea31ec17
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-31 14:25:09 +0200

    update site-50
    update site-50

--- sites/site-50/SVG-Gibson-Les-Paul-Guitar/mainscript.js
+++ sites/site-50/SVG-Gibson-Les-Paul-Guitar/mainscript.js
@@ -14,6 +14,17 @@ let inputPad = document.getElementById("inputPad");
 let inputGriff1 = document.getElementById("inputGriff1");
 let inputGriff2 = document.getElementById("inputGriff2");
 let inputElectrics = document.getElementById("inputElectric");
+let inputInlays = document.getElementById("inputInlays");
+let inlay1 = document.getElementById("inlay1");
+let inlay2 = document.getElementById("inlay2");
+let inlay3 = document.getElementById("inlay3");
+let inlay4 = document.getElementById("inlay4");
+let inlay5 = document.getElementById("inlay5");
+let inlay6 = document.getElementById("inlay6");
+let inlay7 = document.getElementById("inlay7");
+let inlay8 = document.getElementById("inlay8");
+let inlay9 = document.getElementById("inlay9");
+let inlay10 = document.getElementById("inlay10");
 
 hamb1.style.fill = color2.style.fill;
 hamb2.style.fill = color2.style.fill;
@@ -32,8 +43,18 @@ document.getElementById("btn").addEventListener('click', function() {
     electric1.style.fill = inputElectrics.value;
     electric2.style.fill = electric1.style.fill;
     // electric2.style.fill = inputElectrics.value;
+    inlay1.style.fill = inputInlays.value;
+    inlay2.style.fill = inputInlays.value;
+    inlay3.style.fill = inputInlays.value;
+    inlay4.style.fill = inputInlays.value;
+    inlay5.style.fill = inputInlays.value;
+    inlay6.style.fill = inputInlays.value;
+    inlay7.style.fill = inputInlays.value;
+    inlay8.style.fill = inputInlays.value;
+    inlay9.style.fill = inputInlays.value;
+    inlay10.style.fill = inputInlays.value;
 })
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
