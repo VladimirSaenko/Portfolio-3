b165d28fbedc7c88767cc2ca8b5a1a1469bfe82f
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-02-11 11:03:58 +0200

    Update questions in site-38
    Update questions in site-38

--- sites/site-38/Quiz-Example/scripts/quiz.js
+++ sites/site-38/Quiz-Example/scripts/quiz.js
@@ -43,8 +43,8 @@ const questions = [
 		correctOption: "optionA"
 	},{
 		question: "_____ is the hottest Continent on Earth ?",
-		optionA: "Oceania",
-		optionB: "Antarctica",
+		optionA: "Eurasia",
+		optionB: "South America",
 		optionC: "Africa",
 		optionD: "North America",
 		correctOption: "optionC"
@@ -135,7 +135,7 @@ const questions = [
 	},{
 		question: "How many sides does an hexagon have ?",
 		optionA: "Six",
-		optionB: "Sevene",
+		optionB: "Seven",
 		optionC: "Four",
 		optionD: "Five",
 		correctOption: "optionA"
