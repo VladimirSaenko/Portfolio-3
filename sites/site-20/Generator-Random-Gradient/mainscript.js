d982ed6186555ccfacd1cf6ca98c195e7cef0d5f
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-02-15 23:35:59 +0200

    Update js script in site-"Generator Random Gradient"

--- sites/site-20/Generator-Random-Gradient/mainscript.js
+++ sites/site-20/Generator-Random-Gradient/mainscript.js
@@ -4,30 +4,35 @@
 let gradientText = document.getElementById('gradient-text');
 let box = document.getElementById("box");
 let body = document.body;
+
 let directions = [
     "right",
     "left",
     "top",
     "bottom"
 ];
+
+let createGradientButton = document.getElementById("createbtn");
+let copyButton = document.getElementById("copybtn");
+
 box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0, 4)]}` + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
 ',' + getRandomInt(0, 255) + ')' + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
 ',' + getRandomInt(0, 255) + ')' + ')';
 body.style.background = box.style.background;
 gradientText.value = box.style.background;
 
-function getRandomGradient() {
+createGradientButton.addEventListener('click', () => {
     box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0, 4)]}` + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
     ',' + getRandomInt(0, 255) + ')' + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
     ',' + getRandomInt(0, 255) + ')' + ')';
     body.style.background = box.style.background;
     gradientText.value = box.style.background;
-}
+})
 
-function copyPassword() {
+copyButton.addEventListener('click', () => {
     gradientText.select();
     document.execCommand("copy");
-}
+})
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
