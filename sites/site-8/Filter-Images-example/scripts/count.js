cfac4a177e0c5e38863fa1bdb83021f137fc2b76
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-02-20 22:24:43 +0200

    Upd js script site-8
    Upd js script site-8

--- sites/site-8/Filter-Images-example/scripts/count.js
+++ sites/site-8/Filter-Images-example/scripts/count.js
@@ -1,7 +1,7 @@
 
 "use strict";
 
-let countTittle = document.getElementById("count");
+let countTitle = document.getElementById("count");
 let allImages = document.getElementById("all-images");
 let camera = document.getElementById("camera");
 let headphone = document.getElementById("headphone");
@@ -10,46 +10,46 @@ let sho = document.getElementById("shoe");
 let watch = document.getElementById("watch");
 let computer = document.getElementById("computer");
 
-countTittle.textContent = "";
+countTitle.textContent = "";
 
 allImages.addEventListener('click', function() {
-    countTittle.textContent = "";
-    countTittle.style.display = "none";
-    countTittle.style.visibility = "hidden";
+    countTitle.textContent = "";
+    countTitle.style.display = "none";
+    countTitle.style.visibility = "hidden";
 })
 
 camera.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 3 совпадений";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 3 совпадений";
 })
 
 headphone.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 4 совпадений";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 4 совпадений";
 })
 
 mobile.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 5 совпадений";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 5 совпадений";
 })
 
 sho.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 4 совпадения";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 4 совпадения";
 })
 
 watch.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 4 совпадения";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 4 совпадения";
 })
 
 computer.addEventListener('click', function() {
-    countTittle.style.display = "flex";
-    countTittle.style.visibility = "visible";
-    countTittle.textContent = "Найдено 4 совпадения";
+    countTitle.style.display = "flex";
+    countTitle.style.visibility = "visible";
+    countTitle.textContent = "Найдено 4 совпадения";
 })
