49acd058cc5238edbbb3d3e51ad4404a02d2abfe
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-04-27 12:34:08 +0300

    Add Nazair to site-"Witcher Countries"

--- sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
+++ sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
@@ -12,6 +12,7 @@ let fish = document.getElementById("fish"); // Kerack principality
 let lion = document.getElementById("lion") // Cintra kingdom
 let dragon = document.getElementById("dragon") // Zerrikania kingdom
 let temerianLily = document.getElementById("temeria"); // Temeria kingdom
+let nazairRose = document.getElementById("nazairRose"); // Nazair
 
 let path8386 = document.getElementById("path8386") // lily Temerian
 let path8404 = document.getElementById("path8404"); // lily Temerian
@@ -31,19 +32,33 @@ let title7 = document.getElementById("title7");
 let title8 = document.getElementById("title8");
 let title9 = document.getElementById("title9");
 let title10 = document.getElementById("title10");
+let title11 = document.getElementById("title11");
 
-path8386.style.fill = 'rgb(6, 24, 31)';
-path8404.style.fill = 'rgb(6, 24, 31)';
-path8406.style.fill = 'rgb(6, 24, 31)';
-path8417.style.fill = 'rgb(6, 24, 31)';
+path8386.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
+path8404.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
+path8406.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
+path8417.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
+path8386.style.stroke = '#31e8ff'; // lily Temerian
+path8404.style.stroke = '#31e8ff'; // lily Temerian
+path8406.style.stroke = '#31e8ff'; // lily Temerian
+path8417.style.stroke = '#31e8ff'; // lily Temerian
+path10182.style.fill = '#31e8ff'; // lily Temerian
+path11949.style.fill = '#31e8ff'; // lily Temerian
+path11074.style.fill = '#31e8ff'; // lily Temerian
 
-path8386.style.stroke = '#31e8ff';
-path8404.style.stroke = '#31e8ff';
-path8406.style.stroke = '#31e8ff';
-path8417.style.stroke = '#31e8ff';
-path10182.style.fill = '#31e8ff';
-path11949.style.fill = '#31e8ff';
-path11074.style.fill = '#31e8ff';
+function returnColorsTemerianLily() {
+    path8386.style.fill = 'rgb(6, 24, 31)';
+    path8404.style.fill = 'rgb(6, 24, 31)';
+    path8406.style.fill = 'rgb(6, 24, 31)';
+    path8417.style.fill = 'rgb(6, 24, 31)';
+    path8386.style.stroke = '#31e8ff';
+    path8404.style.stroke = '#31e8ff';
+    path8406.style.stroke = '#31e8ff';
+    path8417.style.stroke = '#31e8ff';
+    path10182.style.fill = '#31e8ff';
+    path11949.style.fill = '#31e8ff';
+    path11074.style.fill = '#31e8ff';
+}
 
 function returnBg() {
     body.style.background = "#06181f";
@@ -54,11 +69,16 @@ unicorn.addEventListener('mousemove', function() {
     title1.style.visibility = "visible";
     title1.innerHTML = "kaedwen";
     title1.style.color = "black";
+    path8386.style.fill = 'yellow';
+    path8404.style.fill = 'yellow';
+    path8406.style.fill = 'yellow';
+    path8417.style.fill = 'yellow';
 })
 
 unicorn.addEventListener('mouseout', function() {
     returnBg();
     title1.style.visibility = "hidden";
+    returnColorsTemerianLily();
 })
 
 eagle.addEventListener("mousemove",function() {
@@ -66,11 +86,16 @@ eagle.addEventListener("mousemove",function() {
     title2.style.visibility = "visible";
     title2.style.color = "white";
     title2.innerHTML = "redania";
+    path8386.style.fill = 'red';
+    path8404.style.fill = 'red';
+    path8406.style.fill = 'red';
+    path8417.style.fill = 'red';
 })
 
 eagle.addEventListener('mouseout',function() {
     returnBg();
     title2.style.visibility = "hidden";
+    returnColorsTemerianLily();
 })
 
 sun.addEventListener('mousemove', function() {
@@ -90,11 +115,16 @@ drakkar.addEventListener('mousemove', function() {
     title4.style.visibility = "visible";
     title4.innerHTML = "skellige";
     title4.style.color = "white";
+    path8386.style.fill = 'purple';
+    path8404.style.fill = 'purple';
+    path8406.style.fill = 'purple';
+    path8417.style.fill = 'purple';
 })
 
 drakkar.addEventListener('mouseout', function() {
     returnBg();
     title4.style.visibility = "hidden";
+    returnColorsTemerianLily();
 })
 
 helmet.addEventListener('mousemove', function() {
@@ -126,11 +156,16 @@ fish.addEventListener('mousemove', function() {
     title7.style.visibility = 'visible';
     title7.innerText = "Kerack";
     title7.style.color = "black";
+    path8386.style.fill = 'yellow';
+    path8404.style.fill = 'yellow';
+    path8406.style.fill = 'yellow';
+    path8417.style.fill = 'yellow';
 })
 
 fish.addEventListener("mouseleave", function() {
     returnBg();
     title7.style.visibility = 'hidden';
+    returnColorsTemerianLily();
 })
 
 lion.addEventListener('mousemove', () => {
@@ -138,11 +173,16 @@ lion.addEventListener('mousemove', () => {
     title8.style.visibility = 'visible';
     title8.innerText = "Cintra";
     title8.style.color = "white";
+    path8386.style.fill = 'blue';
+    path8404.style.fill = 'blue';
+    path8406.style.fill = 'blue';
+    path8417.style.fill = 'blue';
 })
 
 lion.addEventListener('mouseleave', () => {
     returnBg();
     title8.style.visibility = 'hidden';
+    returnColorsTemerianLily();
 })
 
 dragon.addEventListener('mousemove', () => {
@@ -150,11 +190,16 @@ dragon.addEventListener('mousemove', () => {
     title9.style.visibility = 'visible';
     title9.innerText = "Zerrikania";
     title9.style.color = "white";
+    path8386.style.fill = 'red';
+    path8404.style.fill = 'red';
+    path8406.style.fill = 'red';
+    path8417.style.fill = 'red';
 })
 
 dragon.addEventListener('mouseleave', () => {
     title9.style.visibility = 'hidden';
     returnBg();
+    returnColorsTemerianLily();
 })
 
 temerianLily.addEventListener('mousemove', () => {
@@ -165,17 +210,18 @@ temerianLily.addEventListener('mousemove', () => {
 })
 
 temerianLily.addEventListener('mouseleave', () => {
-    path8386.style.fill = 'rgb(6, 24, 31)';
-    path8404.style.fill = 'rgb(6, 24, 31)';
-    path8406.style.fill = 'rgb(6, 24, 31)';
-    path8417.style.fill = 'rgb(6, 24, 31)';
-    path8386.style.stroke = '#31e8ff';
-    path8404.style.stroke = '#31e8ff';
-    path8406.style.stroke = '#31e8ff';
-    path8417.style.stroke = '#31e8ff';
-    path10182.style.fill = '#31e8ff';
-    path11949.style.fill = '#31e8ff';
-    path11074.style.fill = '#31e8ff';
     title10.style.visibility = 'hidden';
+    returnBg();
+})
+
+nazairRose.addEventListener('mousemove', () => {
+    body.style.background = '#000';
+    title11.style.visibility = 'visible';
+    title11.innerText = 'Nazair';
+    title11.style.color = 'white';
+})
+
+nazairRose.addEventListener('mouseleave', () => {
+    title11.style.visibility = 'hidden';
     returnBg();
 })
