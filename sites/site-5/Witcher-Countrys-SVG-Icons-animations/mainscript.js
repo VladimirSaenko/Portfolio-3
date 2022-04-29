7e9584aa312558ce2252d678052b12479d078186
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-04-29 13:12:47 +0300

    Add Elves/Dol Blathanna to site-"Witcher Countries SVG"
    Add Elves/Dol Blathanna

--- sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
+++ sites/site-5/Witcher-Countrys-SVG-Icons-animations/mainscript.js
@@ -2,17 +2,18 @@
 "use strict";
 
 let body = document.body;
-let unicorn = document.getElementById("kaedwen"); // Kaedwen kingdom
-let sun = document.getElementById("nilfgaard"); // Nilfgaard empire
-let eagle = document.getElementById("redania"); // Redania kingdom
-let drakkar = document.getElementById("drakkar"); // Skellige
-let helmet = document.getElementById("helmet"); // Toussaint principality
-let axes = document.getElementById("axes"); // Mahakam
-let fish = document.getElementById("fish"); // Kerack principality
-let lion = document.getElementById("lion") // Cintra kingdom
-let dragon = document.getElementById("dragon") // Zerrikania kingdom
-let temerianLily = document.getElementById("temeria"); // Temeria kingdom
-let nazairRose = document.getElementById("nazairRose"); // Nazair
+const unicorn = document.getElementById("kaedwen"); // Kaedwen kingdom
+const sun = document.getElementById("nilfgaard"); // Nilfgaard empire
+const eagle = document.getElementById("redania"); // Redania kingdom
+const drakkar = document.getElementById("drakkar"); // Skellige
+const helmet = document.getElementById("helmet"); // Toussaint principality
+const axes = document.getElementById("axes"); // Mahakam
+const fish = document.getElementById("fish"); // Kerack principality
+const lion = document.getElementById("lion") // Cintra kingdom
+const dragon = document.getElementById("dragon") // Zerrikania
+const temerianLily = document.getElementById("temeria"); // Temeria kingdom
+const nazairRose = document.getElementById("nazairRose"); // Nazair
+const elvesArrows = document.getElementById("arrows"); // Elves/Dol Blathanna
 
 let path8386 = document.getElementById("path8386") // lily Temerian
 let path8404 = document.getElementById("path8404"); // lily Temerian
@@ -21,18 +22,20 @@ let path8417 = document.getElementById("path8417"); // lily Temerian
 let path10182 = document.getElementById("path10182"); // lily Temerian
 let path11949 = document.getElementById("path11949"); // lily Temerian
 let path11074 = document.getElementById("path11074"); // lily Temerian
+let path10190 = document.getElementById("path10190"); // lily Temerian
 
-let title1 = document.getElementById("title1");
-let title2 = document.getElementById("title2");
-let title3 = document.getElementById("title3");
-let title4 = document.getElementById("title4");
-let title5 = document.getElementById("title5");
-let title6 = document.getElementById("title6");
-let title7 = document.getElementById("title7");
-let title8 = document.getElementById("title8");
-let title9 = document.getElementById("title9");
-let title10 = document.getElementById("title10");
-let title11 = document.getElementById("title11");
+let title1 = document.getElementById("title1"); // Kaedwen
+let title2 = document.getElementById("title2"); // Nilfgaard
+let title3 = document.getElementById("title3"); // Redania
+let title4 = document.getElementById("title4"); // Skellige
+let title5 = document.getElementById("title5"); // Toussaint
+let title6 = document.getElementById("title6"); // Mahakam
+let title7 = document.getElementById("title7"); // Kerack
+let title8 = document.getElementById("title8"); // Cintra
+let title9 = document.getElementById("title9"); // Zerrikania
+let title10 = document.getElementById("title10"); // Temeria
+let title11 = document.getElementById("title11"); // Nazair
+let title12 = document.getElementById("title12"); // Elves/Dol Blathanna
 
 path8386.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
 path8404.style.fill = 'rgb(6, 24, 31)'; // lily Temerian
@@ -45,6 +48,7 @@ path8417.style.stroke = '#31e8ff'; // lily Temerian
 path10182.style.fill = '#31e8ff'; // lily Temerian
 path11949.style.fill = '#31e8ff'; // lily Temerian
 path11074.style.fill = '#31e8ff'; // lily Temerian
+path10190.style.fill = 'rgb(49, 232, 255)'; // lily Temerian
 
 function returnColorsTemerianLily() {
     path8386.style.fill = 'rgb(6, 24, 31)';
@@ -67,7 +71,6 @@ function returnBg() {
 unicorn.addEventListener('mousemove', function() {
     body.style.background = "yellow";
     title1.style.visibility = "visible";
-    title1.innerHTML = "kaedwen";
     title1.style.color = "black";
     path8386.style.fill = 'yellow';
     path8404.style.fill = 'yellow';
@@ -85,7 +88,6 @@ eagle.addEventListener("mousemove",function() {
     body.style.background = "red";
     title2.style.visibility = "visible";
     title2.style.color = "white";
-    title2.innerHTML = "redania";
     path8386.style.fill = 'red';
     path8404.style.fill = 'red';
     path8406.style.fill = 'red';
@@ -102,7 +104,6 @@ sun.addEventListener('mousemove', function() {
     body.style.background = "black"
     title3.style.visibility = "visible";
     title3.style.color = "yellow";
-    title3.innerHTML = "nilfgaard";
 })
 
 sun.addEventListener('mouseout', function() {
@@ -113,7 +114,6 @@ sun.addEventListener('mouseout', function() {
 drakkar.addEventListener('mousemove', function() {
     body.style.background = "purple";
     title4.style.visibility = "visible";
-    title4.innerHTML = "skellige";
     title4.style.color = "white";
     path8386.style.fill = 'purple';
     path8404.style.fill = 'purple';
@@ -130,7 +130,6 @@ drakkar.addEventListener('mouseout', function() {
 helmet.addEventListener('mousemove', function() {
     body.style.background = "black";
     title5.style.visibility = "visible";
-    title5.innerHTML = "Toussaint";
     title5.style.color = "yellow";
 })
 
@@ -142,7 +141,6 @@ helmet.addEventListener('mouseout', function() {
 axes.addEventListener('mousemove', function() {
     body.style.background = "black";
     title6.style.visibility = 'visible';
-    title6.innerText = "Mahakam";
     title5.style.color = "white";
 })
 
@@ -154,7 +152,6 @@ axes.addEventListener('mouseleave', function() {
 fish.addEventListener('mousemove', function() {
     body.style.background = "yellow";
     title7.style.visibility = 'visible';
-    title7.innerText = "Kerack";
     title7.style.color = "black";
     path8386.style.fill = 'yellow';
     path8404.style.fill = 'yellow';
@@ -171,7 +168,6 @@ fish.addEventListener("mouseleave", function() {
 lion.addEventListener('mousemove', () => {
     body.style.background = "blue";
     title8.style.visibility = 'visible';
-    title8.innerText = "Cintra";
     title8.style.color = "white";
     path8386.style.fill = 'blue';
     path8404.style.fill = 'blue';
@@ -188,7 +184,6 @@ lion.addEventListener('mouseleave', () => {
 dragon.addEventListener('mousemove', () => {
     body.style.background = "rgb(199, 36, 36)";
     title9.style.visibility = 'visible';
-    title9.innerText = "Zerrikania";
     title9.style.color = "white";
     path8386.style.fill = 'red';
     path8404.style.fill = 'red';
@@ -205,7 +200,6 @@ dragon.addEventListener('mouseleave', () => {
 temerianLily.addEventListener('mousemove', () => {
     body.style.background = 'black';
     title10.style.visibility = 'visible';
-    title10.innerText = 'Temeria';
     title10.style.color = 'white';
 })
 
@@ -217,11 +211,27 @@ temerianLily.addEventListener('mouseleave', () => {
 nazairRose.addEventListener('mousemove', () => {
     body.style.background = '#000';
     title11.style.visibility = 'visible';
-    title11.innerText = 'Nazair';
     title11.style.color = 'white';
 })
 
 nazairRose.addEventListener('mouseleave', () => {
     title11.style.visibility = 'hidden';
     returnBg();
+})
+
+elvesArrows.addEventListener('mousemove', () => {
+    body.style.background = 'green';
+    title12.style.visibility = 'visible';
+    title12.style.color = 'white';
+    // title12.style.color = 'black';
+    path8386.style.fill = 'green';
+    path8404.style.fill = 'green';
+    path8406.style.fill = 'green';
+    path8417.style.fill = 'green';
+})
+
+elvesArrows.addEventListener('mouseleave', () => {
+    title12.style.visibility = 'hidden';
+    returnBg();
+    returnColorsTemerianLily();
 })
