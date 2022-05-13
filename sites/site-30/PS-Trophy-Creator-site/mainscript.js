0e43f73ceaa9e6e4a15cf5647028e559ffdd71b2
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-05-13 17:26:52 +0300

    Update styles site-30
    Update styles site-"PS-Trophy-Creator"

--- sites/site-30/PS-Trophy-Creator-site/mainscript.js
+++ sites/site-30/PS-Trophy-Creator-site/mainscript.js
@@ -7,53 +7,47 @@ let trophytypeInput = document.getElementById("trophytype");
 let trophyImg = document.getElementById("trophyImg");
 let trophyName = document.getElementById("trophyName");
 let mainbutton = document.getElementById("mainbutton");
-trophynameInput.focus();
-// trophytypeInput.focus();
 
 mainbutton.addEventListener("click", function() {
-    trophyName.innerText = trophynameInput.value;
-    getTrophyInType();
-    trophyIcon.classList.add('active');
-
     if(trophynameInput.value == '') {
         trophyName.innerText = "Empty";
     }
+    else {
+        trophyName.innerText = trophynameInput.value;
+    }
+    getTrophyInType();
+    trophyIcon.classList.toggle('active');
+    setTimeout(() => {
+        trophyIcon.classList.toggle('active');
+    }, 2500)
 })
 
 function getTrophyInType() {
     if(trophytypeInput.value == "") {
-        let random = getRandomInt(1,5);
-
-        if(random == 1 || random == 5) {
+        let random = getRandomInt(1, 5);
+        if(random == 1 || random == 4) {
             trophyImg.src = "images/bronze-trophy.png";
         }
-
         if(random == 2) {
             trophyImg.src = "images/silver-trophy.png";
         }
-
         if(random == 3) {
             trophyImg.src = "images/gold-trophy.png";
         }
-        
-        if(random == 4) {
+        if(random == 5) {
             trophyImg.src = "images/platinum-trophy.png";
         }
     }
-
     else {
         if (trophytypeInput.value == "Бронза") {
             trophyImg.src = "images/bronze-trophy.png";
         }
-
         if (trophytypeInput.value == "Серебро") {
             trophyImg.src = "images/silver-trophy.png";
         }
-
         if (trophytypeInput.value == "Золото") {
             trophyImg.src = "images/gold-trophy.png";
         }
-
         if (trophytypeInput.value == "Платина") {
             trophyImg.src = "images/platinum-trophy.png";
         }
