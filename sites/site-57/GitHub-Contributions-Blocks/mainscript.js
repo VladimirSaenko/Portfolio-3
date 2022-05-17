cbd283a1db32f5fba097520c1dfaa8e5ea07bbbe
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-05-17 12:50:46 +0300

    add 2 new colors site-"GitHub Contributions Blocks"
    add 2 new colors site-"GitHub Contributions Blocks"

--- sites/site-57/GitHub-Contributions-Blocks/mainscript.js
+++ sites/site-57/GitHub-Contributions-Blocks/mainscript.js
@@ -41,6 +41,7 @@ colorlist.addEventListener('input', () => {
     switch (colorlist.value) {
         case 'halloween':
             colorlist.style.color = 'orange';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
             document.documentElement.style.setProperty('--level1', 'rgb(98, 28, 2)');
             document.documentElement.style.setProperty('--level2', 'rgb(190, 85, 28)');
             document.documentElement.style.setProperty('--level3', 'rgb(249, 122, 29)');
@@ -48,6 +49,7 @@ colorlist.addEventListener('input', () => {
         break;
         case 'office':
             colorlist.style.color = 'blue';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
             document.documentElement.style.setProperty('--level1', 'rgb(4, 20, 82)');
             document.documentElement.style.setProperty('--level2', 'rgb(7, 18, 134)');
             document.documentElement.style.setProperty('--level3', 'rgb(38, 30, 193)');
@@ -55,6 +57,7 @@ colorlist.addEventListener('input', () => {
         break;
         case 'angry':
             colorlist.style.color = 'red';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
             document.documentElement.style.setProperty('--level1', 'rgb(82, 8, 4)');
             document.documentElement.style.setProperty('--level2', 'rgb(134, 7, 7)');
             document.documentElement.style.setProperty('--level3', 'rgb(192, 14, 14)');
@@ -62,14 +65,32 @@ colorlist.addEventListener('input', () => {
         break;
         case 'windu':
             colorlist.style.color = 'purple';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
             document.documentElement.style.setProperty('--level1', 'rgb(76, 4, 82)');
             document.documentElement.style.setProperty('--level2', 'rgb(134, 7, 117)');
             document.documentElement.style.setProperty('--level3', 'rgb(192, 14, 186)');
             document.documentElement.style.setProperty('--level4', 'rgb(250, 1, 246)');
         break;
+        case 'standard2':
+            colorlist.style.color = 'rgb(0, 172, 66)';
+            document.documentElement.style.setProperty('--level0', 'rgb(238, 238, 238)');
+            document.documentElement.style.setProperty('--level1', 'rgb(205, 237, 130)');
+            document.documentElement.style.setProperty('--level2', 'rgb(113, 206, 101)');
+            document.documentElement.style.setProperty('--level3', 'rgb(0, 172, 66)');
+            document.documentElement.style.setProperty('--level4', 'rgb(0, 110, 37)');
+        break;
+        case 'bananawolf':
+            colorlist.style.color = 'yellow';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
+            document.documentElement.style.setProperty('--level1', 'rgb(98, 101, 11)');
+            document.documentElement.style.setProperty('--level2', 'rgb(186, 186, 14)');
+            document.documentElement.style.setProperty('--level3', 'rgb(231, 221, 36)');
+            document.documentElement.style.setProperty('--level4', 'gold');
+        break;
         default:
             colorlist.style.color = 'greenyellow';
             // colorlist.style.color = 'rgb(59, 207, 93)';
+            document.documentElement.style.setProperty('--level0', 'rgb(63, 70, 78)');
             document.documentElement.style.setProperty('--level1', 'rgb(4, 76, 39)');
             document.documentElement.style.setProperty('--level2', 'rgb(0, 111, 52)');
             document.documentElement.style.setProperty('--level3', 'rgb(46, 163, 74)');
