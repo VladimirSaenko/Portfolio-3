63c4f67f0263ff33888b73b2d8e8de54aa7af327
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-11-09 02:00:09 +0200

    delete audio site-7
    delete audio site-7

--- sites/site-7/Second-version-Calculator/scripts/audio.js
+++ sites/site-7/Second-version-Calculator/scripts/audio.js
@@ -1,22 +1,11 @@
-
 "use strict";
 
 let musicbutt = document.getElementById('musicbutt-3');
 musicbutt.addEventListener('click',function () {
-    document.getElementById('sound-3').play();
+    document.getElementById('sound3').play();
 });
 
 let musicbutt40 = document.getElementById('musicbutt40');
 musicbutt40.addEventListener('click',function () {
     document.getElementById('sound40').play();
-});
-
-let musicButt0_25 = document.getElementById('alc-1');
-musicButt0_25.addEventListener('click', function() {
-    document.getElementById('alcosound').play();
-});
-
-let musicButt0_5 = document.getElementById('alc-2');
-musicButt0_5.addEventListener('click', function() {
-    document.getElementById('alcosound').play();
 });
