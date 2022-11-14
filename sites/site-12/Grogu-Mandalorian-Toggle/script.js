35eec722f4433ed80d6da0a5986f04470bd8a805
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-11-14 07:31:14 +0200

    baby yoda

--- sites/site-12/Grogu-Mandalorian-Toggle/script.js
+++ sites/site-12/Grogu-Mandalorian-Toggle/script.js
@@ -4,14 +4,12 @@
 let mando = document.getElementById("mando");
 let container = document.getElementById("container");
 let toggle = document.getElementById("slider");
-let gitSupp = document.getElementById("gitSupp");
 let waySpan = document.getElementById("way");
 let isActive = false;
 
 function activateMando() {
     if(!isActive) {
         mando.style.visibility = 'visible';
-        gitSupp.style.visibility = 'visible';
         container.style.background = '#121011';
         toggle.style.backgroundImage = "linear-gradient(#000000,#5a5252,#585552, #6d6565, #000000)";
         waySpan.style.opacity = 1;
@@ -19,7 +17,6 @@ function activateMando() {
     }
     else if(isActive) {
         mando.style.visibility = 'hidden';
-        gitSupp.style.visibility = 'hidden';
         container.style.background = '#5ec4e0';
         toggle.style.backgroundImage = "linear-gradient(#aefaa7, #00ff22, #1eff00, #33ff00, #aefda7)";
         waySpan.style.opacity = 0;
