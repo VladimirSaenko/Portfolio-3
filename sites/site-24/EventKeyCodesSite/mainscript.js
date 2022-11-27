b04703dd3227b42a7f8bf03f3fb66a48bfd19e37
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-11-27 16:04:28 +0200

    Update site-24
    Update site-"Event Key Codes site"

--- sites/site-24/EventKeyCodesSite/mainscript.js
+++ sites/site-24/EventKeyCodesSite/mainscript.js
@@ -3,6 +3,7 @@
 
 const insert = document.getElementById("insert");
 const body = document.body;
+const keys = document.querySelectorAll('.keyboard-row button');
 
 window.addEventListener('keydown', (event) => {
     insert.innerHTML = `
@@ -24,6 +25,28 @@ window.addEventListener('keydown', (event) => {
     `;
 
     body.style.background = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`
+})
+
+keys.forEach(key => {
+    key.onclick = () => {
+        insert.innerHTML = `
+        <div class='key'>
+            ${key.getAttribute('data-key')}
+            <small>event.key</small>
+        </div>
+    
+        <div class='key'>
+        ${key.getAttribute('data-keycode')}
+        <small>event.keyCode</small>
+        </div>
+    
+        <div class='key'>
+        ${key.getAttribute('data-key')}
+        <small>event.code</small>
+        </div>
+        `;
+    body.style.background = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`
+    }
 })
 
 function getRandomInt(min, max) {
