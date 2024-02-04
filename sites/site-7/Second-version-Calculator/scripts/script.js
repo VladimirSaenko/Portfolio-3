3174663ab4efb0e005a4b5ffcebd089e3095ec50
witcherwow240 <vova.saenko1@gmail.com>
2024-02-04 22:04:50 +0200

    upd site-"Calculator 2"
    upd site-"Calculator 2"

---
+++ sites/site-7/Second-version-Calculator/scripts/script.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelector(".container"), {
+    max: 15,
+    speed: 400,
+    glare: true,
+    "max-glare":0.2
+});
+
+//It also supports NodeList
+// VanillaTilt.init(document.querySelectorAll(".your-element"));
